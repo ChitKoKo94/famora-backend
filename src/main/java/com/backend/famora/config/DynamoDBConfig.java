d46799591312ffd1d1e0b6c5234d2e7c1f1919eb
CKK <takenheart7@gmail.com>
2025-05-14 23:12:54 -0500

    Default bean for DynamoDB client

--- src/main/java/com/backend/famora/config/DynamoDBConfig.java
+++ src/main/java/com/backend/famora/config/DynamoDBConfig.java
@@ -1,6 +1,7 @@
 package com.backend.famora.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -48,5 +49,11 @@ public class DynamoDBConfig {
                 .region(Region.of(awsRegion))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(DynamoDbAsyncClient.class)
+    public DynamoDbAsyncClient fallbackClient() {
+        return DynamoDbAsyncClient.builder().build();
     }
 }
